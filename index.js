9f7c533695cc50a4a7e0e373ebc69fb55aab166e
Galdin Raphael <gldraphael@gmail.com>
2016-11-22 02:25:37 +0530

    Perform checks to the number of  arguments

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ process.on('uncaughtException', function(error) {
 // Remove the first two paths
 var args = process.argv.slice(2);
 
+if(args.length == 0) {
+  console.log('Syntax: chordsheet <input-file> [output-file]');
+  process.exit(1);
+} else if(args.length > 2) {
+  console.error('Too many arguments found');
+  console.log('Syntax: chordsheet <input-file> [output-file]');
+  process.exit(1);
+}
+
 // Assume the next path is the input file to be processed
 var input = args[0];
 
