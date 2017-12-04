b74c1c2cb24d59f314d82e92047955d37e33d884
Galdin Raphael <gldraphael@gmail.com>
2017-12-05 02:24:03 +0530

    Add more elaborate regex test

--- tests/chord-regex.js
+++ tests/chord-regex.js
@@ -2,10 +2,29 @@ const chordRegex = require('../chord-regex')
 const tape = require('tape')
 
 tape('regex test', function (test) {
-  test.plan(1)
 
-  const myChord = "[C]"
-  var match = chordRegex.exec(myChord)[1]
+  let chords = [
+    'C', 'D', 'E', 'F', 'G', 'A', 'B', // white keys
+    'C#', 'Eb', 'F#', 'Ab', 'G#', 'Bb', // sharps and flats
+    'Db', 'D#', 'Gb', 'A#', // for those who like to use non-canon names for sharps and flats
+    'Cm', 'Cm7', 'CM7', 'Cmaj7', 'CmM7', // minors and 7ths
+    'C6','C9', 'C11', 'C13', // few more extension chords
+    'Caug', 'Cdim', 'Csus', 'Csus2', 'Csus4',
+    'C/E', 'D/F#' // Slash chords
+  ]
 
-  test.equal(match, 'C')
+  console.log(chords.length)
+  test.plan(chords.length)
+
+  chords.forEach(chord => {
+    const input = '[' + chord + ']'
+    if(chordRegex.exec(input) == null || chordRegex.exec(input).length == 0) {
+      test.fail('Failed for input: ' + input)
+    }
+    else {
+      const match = chordRegex.exec(input)[1]
+      test.equal(match, chord)
+    }
+  });
+
 });
