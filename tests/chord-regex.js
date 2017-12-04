f1cde97e5eb6c0af3d734e948b3cbfde21a90487
Galdin Raphael <gldraphael@gmail.com>
2017-12-05 02:40:19 +0530

    Fix linting errors
    Also fix the lint npm-script command and add a lint.fix npm script

--- tests/chord-regex.js
+++ tests/chord-regex.js
@@ -2,13 +2,12 @@ const chordRegex = require('../chord-regex')
 const tape = require('tape')
 
 tape('regex test', function (test) {
-
   let chords = [
     'C', 'D', 'E', 'F', 'G', 'A', 'B', // white keys
     'C#', 'Eb', 'F#', 'Ab', 'G#', 'Bb', // sharps and flats
     'Db', 'D#', 'Gb', 'A#', // for those who like to use non-canon names for sharps and flats
     'Cm', 'Cm7', 'CM7', 'Cmaj7', 'CmM7', // minors and 7ths
-    'C6','C9', 'C11', 'C13', // few more extension chords
+    'C6', 'C9', 'C11', 'C13', // few more extension chords
     'Caug', 'Cdim', 'Csus', 'Csus2', 'Csus4',
     'C/E', 'D/F#' // Slash chords
   ]
@@ -18,13 +17,11 @@ tape('regex test', function (test) {
 
   chords.forEach(chord => {
     const input = '[' + chord + ']'
-    if(chordRegex.exec(input) == null || chordRegex.exec(input).length == 0) {
+    if (chordRegex.exec(input) === null || chordRegex.exec(input).length === 0) {
       test.fail('Failed for input: ' + input)
-    }
-    else {
+    } else {
       const match = chordRegex.exec(input)[1]
       test.equal(match, chord)
     }
-  });
-
-});
+  })
+})
