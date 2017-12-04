1af2d1ac4546634368f8438f320e836a9ab4aadb
Galdin Raphael <gldraphael@gmail.com>
2017-12-05 02:04:58 +0530

    Add linting and fix linting errors

--- index.js
+++ index.js
@@ -10,34 +10,33 @@ const chordPattern = mdRegex(
   chordRegex,
 
   // this function will be called when something's in square brackets
-  function(match, utils) {
-    return '<span class="chord"><span class="inner">' + match[1] + '</span></span>';
+  function (match, utils) {
+    return '<span class="chord"><span class="inner">' + match[1] + '</span></span>'
   }
 )
 
 // Import markdown-it and configure it to use our chordpattern
 const md = require('markdown-it')({
-  html:         false,        // Enable HTML tags in source
-  xhtmlOut:     true,         // Use '/' to close single tags (<br />).
-                              // This is only for full CommonMark compatibility.
-  breaks:       true,         // Convert '\n' in paragraphs into <br>
-  linkify:      true,         // Autoconvert URL-like text to links
+  html: false, // Enable HTML tags in source
+  xhtmlOut: true, // Use '/' to close single tags (<br />).
+  // This is only for full CommonMark compatibility.
+  breaks: true, // Convert '\n' in paragraphs into <br>
+  linkify: true, // Autoconvert URL-like text to links
 
   // Enable some language-neutral replacement + quotes beautification
-  typographer:  false,
+  typographer: false,
 
   // Double + single quotes replacement pairs, when typographer enabled,
   // and smartquotes on. Could be either a String or an Array.
   quotes: '“”‘’'
 }).use(chordPattern)
 
-
 module.exports = {
 
   // Our exported function
   toHtml: function (chordMarkdownText) {
-    if(chordMarkdownText === undefined) {
-      throw new Error('Argument chordMarkdownText is required.');
+    if (chordMarkdownText === undefined) {
+      throw new Error('Argument chordMarkdownText is required.')
     }
     return md.render(chordMarkdownText)
   }
