81da0cbfb68ab5203ef53c5f591a289e76820719
Galdin Raphael <gldraphael@gmail.com>
2017-12-05 09:52:50 +0530

    Add alt names for 7th chords

--- tests/chord-regex.js
+++ tests/chord-regex.js
@@ -6,13 +6,12 @@ tape('Regex against valid chords', function (test) {
     'C', 'D', 'E', 'F', 'G', 'A', 'B', // white keys
     'C#', 'Eb', 'F#', 'Ab', 'G#', 'Bb', // sharps and flats
     'Db', 'D#', 'Gb', 'A#', // for those who like to use non-canon names for sharps and flats
-    'Cm', 'Cm7', 'CM7', 'Cmaj7', 'CmM7', // minors and 7ths
+    'Cm', 'Caug', 'Cdim', 'Csus', 'Csus2', 'Csus4', // a few more triads
+    'Cm7', 'CM7', 'Cmaj7', 'CmM7', // 7ths
     'C6', 'C9', 'C11', 'C13', // few more extension chords
-    'Caug', 'Cdim', 'Csus', 'Csus2', 'Csus4',
     'C/E', 'D/F#' // Slash chords
   ]
 
-  console.log(chords.length)
   test.plan(chords.length)
 
   chords.forEach(chord => {
