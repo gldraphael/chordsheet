fe7c43c2447476a85c030c02aeaebda8ae098abf
Galdin Raphael <gldraphael@users.noreply.github.com>
2017-12-19 12:27:41 +0530

    ✅ Update regex to handle a few edgecases
    Add tests accordingly

--- src/chord-regex.ts
+++ src/chord-regex.ts
@@ -1,7 +1,7 @@
 const chordNames = `[A-G][#|b]?`
-const qualities = `min|maj|m|M|dim|aug|sus[249]?`
+const qualities = `min|m|dim|aug|sus[249]?`
 
-const degrees = `[#|b|M|maj)?(2|3|4|5|6|7|9|11|13)`
+const degrees = `(#|b|M|maj)?(2|3|4|5|6|7|9|11|13)`
 const extensions = `[\\(]?[add|sub]?${degrees}[\\)]?`
 const slashChordPattern = `\\/${chordNames}`
 
--- tests/chord-regex.ts
+++ tests/chord-regex.ts
@@ -1,6 +1,8 @@
 import { chordRegex } from '../src/chord-regex'
 import * as tape from 'tape'
 
+console.log('Regex: ', chordRegex)
+
 tape('Regex against valid chords', function (test) {
   let chords = [
     'C', 'D', 'E', 'F', 'G', 'A', 'B', // white keys
@@ -10,13 +12,28 @@ tape('Regex against valid chords', function (test) {
     'Cm7', 'CM7', 'C7', 'CmM7', // 7ths
     'Cmin7', 'Cmaj7', 'Cminmaj7', // 7ths with alt names
     'C6', 'C9', 'C11', 'C13', // few more extension chords
-    'C/E', 'D/F#' // Slash chords
+    'C/E', 'D/F#', // Slash chords
+    'C#m(#11b5)', 'C#mb5', // few real extension chords
   ]
 
   test.plan(chords.length)
 
   chords.forEach(chord => {
     test.true(isMatch(chord), chord)
+  })
+})
+
+tape('Regex against invalid chords', function (test) {
+  let invalidChords = [
+    'CMm7', // major minors are dominant 7ths
+    'Cmaj', 'CM', // Just use C
+  ]
+  invalidChords = invalidChords.concat('HIJKLMNOPQRSTUVWXYZ'.split('')) // Invalid chordnames
+
+  test.plan(invalidChords.length)
+
+  invalidChords.forEach(chord => {
+    test.false(isMatch(chord), chord)
   })
 })
 
