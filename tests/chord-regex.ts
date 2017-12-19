b6dfb7cf44ebfe5b84d1d4acd34d22229e295182
Galdin Raphael <gldraphael@users.noreply.github.com>
2017-12-19 13:00:55 +0530

    Add tests for + and o

--- tests/chord-regex.ts
+++ tests/chord-regex.ts
@@ -8,7 +8,8 @@ tape('Regex against valid chords', function (test) {
     'C', 'D', 'E', 'F', 'G', 'A', 'B', // white keys
     'C#', 'Eb', 'F#', 'Ab', 'G#', 'Bb', // sharps and flats
     'Db', 'D#', 'Gb', 'A#', // alt names for sharps and flats
-    'Cm', 'Caug', 'Cdim', 'Csus', 'Csus2', 'Csus4', // C triads with diff qualities
+    'Cm', 'Caug', 'C+', 'Cdim', 'Co', // different qualities
+    'Csus', 'Csus2', 'Csus4', // suspended chords
     'Cm7', 'CM7', 'C7', 'CmM7', // 7ths
     'Cmin7', 'Cmaj7', 'Cminmaj7', // 7ths with alt names
     'C6', 'C9', 'C11', 'C13', // few more extension chords
