a53b99f4934b808a1fa9b198c5ea2cd62d74a71b
Galdin Raphael <gldraphael@users.noreply.github.com>
2017-12-19 13:00:39 +0530

    Accept o and + for dim and aug chords

--- src/chord-regex.ts
+++ src/chord-regex.ts
@@ -1,5 +1,5 @@
 const chordNames = `[A-G][#|b]?`
-const qualities = `min|m|dim|aug|sus[249]?`
+const qualities = `min|m|dim|o|aug|\\+|sus[249]?`
 
 const degrees = `(#|b|M|maj)?(2|3|4|5|6|7|9|11|13)`
 const extensions = `[\\(]?(add|sub)?${degrees}[\\)]?`
