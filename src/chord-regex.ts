4a8a3f3260f16377d9fb5d13083bed6866b58770
Galdin Raphael <gldraphael@users.noreply.github.com>
2017-12-19 12:57:01 +0530

    Fix regex bug with add/sub extensions

--- src/chord-regex.ts
+++ src/chord-regex.ts
@@ -2,7 +2,7 @@ const chordNames = `[A-G][#|b]?`
 const qualities = `min|m|dim|aug|sus[249]?`
 
 const degrees = `(#|b|M|maj)?(2|3|4|5|6|7|9|11|13)`
-const extensions = `[\\(]?[add|sub]?${degrees}[\\)]?`
+const extensions = `[\\(]?(add|sub)?${degrees}[\\)]?`
 const slashChordPattern = `\\/${chordNames}`
 
 const chordRegexString = `(${chordNames})(${qualities})?(${extensions})*(${slashChordPattern})?`
