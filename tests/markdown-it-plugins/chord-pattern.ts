ceaf5961caac68ae44a3397b45b5e0bc5ca63e29
Galdin Raphael <gldraphael@users.noreply.github.com>
2017-12-20 10:08:26 +0530

    Add tests for the chord-pattern plugin

---
+++ tests/markdown-it-plugins/chord-pattern.ts
@@ -0,0 +1,20 @@
+import * as tape from 'tape'
+import { chordPattern } from '../../src/markdown-it-plugins/chord-pattern';
+import { chordRegex } from '../../dist/chord-regex';
+import { defaultChordsheetOptions } from '../../dist/defaults';
+import * as md from 'markdown-it'
+
+tape('chord-pattern plugin test against valid input', function (test) {
+  const chord = 'C'
+  const input = `[${chord}]`
+  const expectedOutput = `<span class="chord"><span class="inner">${chord}</span></span>`
+
+  const markdownIt = md(defaultChordsheetOptions.markdownItOptions as md.Options)
+    .use(chordPattern)
+
+  test.plan(1)
+
+  const output = markdownIt.render(input)
+  const isFound = output.indexOf(expectedOutput) != -1
+  test.true(isFound)
+})
